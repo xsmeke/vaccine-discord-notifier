684b2c95b0b1a5999f66d17cc759d60305c9e001
Ayush Pandey <45521130+xsmeke@users.noreply.github.com>
2021-05-23 10:42:48 +0530

    added-pincode-notification

---
+++ vaccine_do_bc.py
@@ -0,0 +1,61 @@
+import requests
+from pygame import mixer 
+from datetime import datetime, timedelta
+import time
+import discord_notify as dn
+
+notifier = dn.Notifier("https://discord.com/api/webhooks/844969980251603024/gx0QIIqcjpA8-TjLeW1av4-L2Cf1wQtln1Z055WRmSuoh6Tvq4OuoFxEVgILoyWUcDfi")
+
+age = 20
+pincodes = ["273402"]
+num_days = 1
+
+print_flag = 'Y'
+
+print("To chaliye shuru karte hain!")
+
+actual = datetime.today()
+list_format = [actual + timedelta(days=i) for i in range(num_days)]
+actual_dates = [i.strftime("%d-%m-%Y") for i in list_format]
+
+while True:
+    counter = 0   
+
+    for pincode in pincodes:   
+        for given_date in actual_dates:
+
+            URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, given_date)
+            header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'} 
+            
+            result = requests.get(URL, headers=header)
+
+            if result.ok:
+                response_json = result.json()
+                if response_json["centers"]:
+                    if(print_flag.lower() =='y'):
+                        for center in response_json["centers"]:
+                            for session in center["sessions"]:
+                                if (session["min_age_limit"] <= age and session["available_capacity"] > 0 ) :
+                                    print('Pincode: ' + pincode)
+                                    print("Available on: {}".format(given_date))
+                                    print("\t", center["name"])
+                                    print("\t", center["block_name"])
+                                    print("\t Price: ", center["fee_type"])
+                                    print("\t Availablity : ", session["available_capacity"])
+
+                                    if(session["vaccine"] != ''):
+                                        print("\t Vaccine type: ", session["vaccine"])
+                                    print("\n")
+                                    counter = counter + 1
+            else:
+                print("No Response!")
+                
+    if (counter == 0):
+        print("Lawde ka government hai, koi slot nahi hai.")
+    else:
+        notifier.send("Badhai ho, vaccine mili hai!" + pincode + "me", print_message=True)
+
+    dt = datetime.now() + timedelta(minutes=3)
+
+    while datetime.now() < dt:
+        time.sleep(1)
